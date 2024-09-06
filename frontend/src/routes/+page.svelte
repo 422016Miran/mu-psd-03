f0d3d3391c6149f277a3bac043a02493e6e72d18
422016Miran <422016@m.mie-u.ac.jp>
2024-09-06 15:11:15 +0900

    コミット

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,13 +1,55 @@
 <script>
-  let name = "world"; // 変数を宣言する。
+  let name = "Report"; // 変数を宣言する。
+  let questions = [
+    { id: 1, text: '札幌'},
+    { id: 2, text: '東京'},
+    { id: 3, text: '名古屋'},
+    { id: 4, text: '大阪'},
+    { id: 5, text: '福岡'},
+    { id: 6, text: '那覇'}
+  ];
+  let selected;
+
+  // 日付と時間を取得する関数
+  function getCurrentDateTime() {
+    const now = new Date();
+    const date = now.toLocaleDateString();
+    const time = now.toLocaleTimeString();
+    return `${date} ${time}`;
+  }
+
+  // 初期化時に日付と時間を設定
+  let currentDateTime = getCurrentDateTime();
+
+  // 1秒ごとに日付と時間を更新
+  setInterval(() => {
+    currentDateTime = getCurrentDateTime();
+  }, 1000);
 </script>
 
 <main>
-  <h1 class="title">This is  My Frontend Server.</h1>
-  <div>hello {name},{name}</div>
+  <h1 class="title">Welcome to Weather Report!</h1>
+  <div>Weather {name}</div>
   <!-- 変数が描画される -->
   <div />
   <a href="/getdata">get data page</a>
+
+  <p>地名を選んでください</p>
+  <form on:submit>
+    <select bind:value={selected}>
+      {#each questions as question}
+        <option value={question}>
+          {question.text}
+        </option>
+      {/each}
+    </select>
+    <br>
+    <button type="submit">検索</button>
+  </form>
+  <p>検索した地名: {selected ? selected.text : '[waiting...]'}</p>
+  ------------------------------------------------------------------
+  <h3>【使い方】</h3>
+  <p>現在の日時: {currentDateTime}</p>
 </main>
 
 <style>
@@ -15,5 +57,14 @@
   .title {
     color: snow;
     background-color: blue;
+  }
+  select {
+    text-align: center;
+    width: 85px;
+    height: 30px;
+  }
+  button {
+    width: 85px;
+    max-width: 100%;
   }
 </style>
